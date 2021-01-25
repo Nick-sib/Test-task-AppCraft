145b5b3d480ce355567af84a5c43b1ee91569cd4
nick-sib <khc1981@mail.ru>
2021-01-25 12:06:12 +0700

    Music Service

--- app/src/main/java/com/nick_sib/testtaskappcraft/ui/fragments/ServicesFragment.kt
+++ app/src/main/java/com/nick_sib/testtaskappcraft/ui/fragments/ServicesFragment.kt
@@ -1,9 +1,37 @@
 package com.nick_sib.testtaskappcraft.ui.fragments
 
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.nick_sib.testtaskappcraft.R
+import com.nick_sib.testtaskappcraft.databinding.FragmentAlbumsListBinding
+import com.nick_sib.testtaskappcraft.databinding.FragmentNavigationBinding
 
-class ServicesFragment: Fragment(R.layout.fragment_navigation) {
+class ServicesFragment: Fragment() {
 
+    private var binding: FragmentNavigationBinding? = null
+    private var snack: Snackbar? = null
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View = FragmentNavigationBinding.inflate(inflater, container, false).let {
+        binding = it
+        it.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
+    }
 }
